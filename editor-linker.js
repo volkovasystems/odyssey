61b13f6d59a26b532b06921bf1a491d6017f93b3
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-02-08 22:49:18 +0800

    Updated editorLinker procedure.

--- editor-linker.js
+++ editor-linker.js
@@ -1,4 +1,18 @@
-
+/*:
+    @include:
+        {
+            "class-linker": "Linker"
+        }
+    @end-include
+    
+    @module-configuration:
+		{
+			"moduleName": "editorLinker",
+			"authorName": "Richeve S. Bebedor",
+			"isGlobal": true
+		}
+	@end-module-configuration
+*/
 editorLinker = function editorLinker( ){
     
 };
